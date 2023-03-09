6f8aaba3dd46c015401a0966e6abda68a47e4153
tuvapersson <80885269+tuvapersson@users.noreply.github.com>
2023-03-09 12:04:34 +0100

    started hamburger

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
 let accordionItems = document.querySelectorAll(".accordion-item")
-console.log(accordionItems)
 
 accordionItems.forEach(item => {
     item.addEventListener('click', () => {
         item.classList.toggle("accordionActive")
       })
 });
+
+let galleryButtons = [...document.querySelectorAll(".gallery-circle")]
+let productImage = document.querySelector(".product-img")
+
+galleryButtons.forEach(item => {
+  item.addEventListener('click', () => {
+    for (let i = 0; i < galleryButtons.length; i++) {
+      galleryButtons[i].style.opacity = 0.7
+    }
+    item.style.opacity = 1;
+      if (item == galleryButtons[0]) {
+        productImage.src = "./img/main.jpg"
+      }
+      else if (item == galleryButtons[1]) {
+        productImage.src = "./img/main2.jpg"
+      }
+      else if (item == galleryButtons[2]) {
+        productImage.src = "./img/main3.jpg"
+      }
+    })
+});
+
+let sizeButtons = [...document.querySelectorAll(".size")]
+
+sizeButtons.forEach(item => {
+  item.addEventListener('click', () => {
+    for (let i = 0; i < sizeButtons.length; i++) {
+      sizeButtons[i].style.backgroundColor = "#fff"
+      sizeButtons[i].style.color = "#000"
+    }
+    item.style.backgroundColor = "#000"
+    item.style.color = "#fff"
+    })
+});
+
+let hamburgerMenu = document.querySelector(".hamburger-menu")
+let hamburgerMenuSpans = [...document.querySelectorAll(".hamburger-span")]
+
+hamburgerMenu.addEventListener('click', toggleHamburger)
+
+function toggleHamburger() {
+  for (let i = 0; i < hamburgerMenuSpans.length; i++) {
+    hamburgerMenuSpans[i].classList.toggle("cross")
+  }
+}
